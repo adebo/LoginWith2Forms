9006b304bd2d05fa89ca478d33361dbe34ef4e80
Antonio Debo <antonio_debo@hotmail.com>
2020-03-22 18:49:06 -0600

    Primer cambio

---
+++ LoginWith2Forms/frmLogin.vb
@@ -0,0 +1,28 @@
+Public Class frmLogin
+
+    ' TODO: inserte el código para realizar autenticación personalizada usando el nombre de usuario y la contraseña proporcionada 
+    ' (Consulte https://go.microsoft.com/fwlink/?LinkId=35339).  
+    ' El objeto principal personalizado se puede adjuntar al objeto principal del subproceso actual como se indica a continuación: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' donde CustomPrincipal es la implementación de IPrincipal utilizada para realizar la autenticación. 
+    ' Posteriormente, My.User devolverá la información de identidad encapsulada en el objeto CustomPrincipal
+    ' como el nombre de usuario, nombre para mostrar, etc.
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        If rbForm1.Checked Then
+            Form1.Show()
+        End If
+        If rbForm2.Checked Then
+            Form2.Show()
+        End If
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rbForm1.Checked = True
+    End Sub
+End Class
